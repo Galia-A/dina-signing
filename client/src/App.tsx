9da55c8e7c989c487856251d7a6a2eec36e7d71c
Galia Appel <galia.appel@gmail.com>
2020-11-16 02:33:55 +0200

    Created homepage.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.scss";
+
+// import Header from "./components/header/header.component";
+// import Footer from "./components/footer/footer.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="he-language">
+      {/* <Header /> */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        {/* <Route path="*" component={PageNotFound} /> */}
+      </Switch>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ client/src/components/courses-preview/courses-preview.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./courses-preview.styles.scss";
+
+const CoursesPreview = () => (
+  <div className="courses-preview page-container">courses-preview</div>
+);
+
+export default CoursesPreview;
