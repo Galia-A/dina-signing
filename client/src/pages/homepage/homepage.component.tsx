4ed0c3255e4f3290f05b38e9e28e4e0ac778f054
Galia Appel <galia.appel@gmail.com>
2020-11-22 02:59:06 +0200

    Added a welcome message.

---
+++ client/src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./homepage.styles.scss";
+import CoursesPreview from "../../components/courses-preview/courses-preview.component";
+
+const HomePage = () => (
+  <div className="homepage page-container">
+    <div className="banner">
+      <div className="profile-picture"></div>
+    </div>
+    <div className="welcome-text">
+      למי שחושבים ששפת הסימנים היא יפה, מופלאה, חשובה וקסומה!
+      <br />
+      מפתחת את המוח ואת יכולת הביטוי!
+      <br />
+      למי שרוצה עולם נגיש יותר, לדבר עם השכן או האחיינית בשפת הסימנים...
+      <br />
+      למי שאוהבים ללמוד בכיף!
+      <br />
+      לקטנים, לגדולים ובמיוחד בשבילך !
+    </div>
+    <CoursesPreview />
+  </div>
+);
+
+export default HomePage;
